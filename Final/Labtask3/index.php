0fae3ff953c7890fccf0e4c76278b172a0e5aba9
Md. Aminul Rasel <57564771+Aminul138@users.noreply.github.com>
2020-12-10 03:17:06 +0600

    Update index.php

--- Final/Labtask3/index.php
+++ Final/Labtask3/index.php
@@ -1,39 +1,49 @@
+
+<?php
+include("connection.php");
+?>
+
 <!DOCTYPE HTML>
 <html>
 <head>
   <title>Person Info</title>
 </head>
 <body>
- <form action="insert.php" method="POST">
-  <table>
+  
+<form action="action.php" method="GET">
+  <center>
+
+     Name : 
+     <input type="text" name="username" required> <br>
     
-   <tr>
-    <td>Name :</td>
-    <td><input type="text" name="username" required></td>
-   </tr>
-   <tr>
-    <td>Password :</td>
-    <td><input type="password" name="password" required></td>
-   </tr>
-   <tr>
-    <td>Gender :</td>
-    <td>
-     <input type="radio" name="gender" value="Male" required>Male
-     <input type="radio" name="gender" value="Femail" required>Female
-    </td>
-   </tr>
-   <tr>
-    <td>Email :</td>
-    <td><input type="email" name="email" required></td>
-   </tr> 
-   <tr>
-    <td>Date of Birth :</td>
-    <td><input type="text" name="Birth" required></td>
-   </tr>
-   <tr>
-    <td><input type="submit" value="Submit"></td>
-   </tr>
-  </table>
- </form>
+    <br>
+     Password : 
+     <input type="password" name="password" required> <br>
+    <br>
+    
+     Gender : 
+     
+     <input type="text" placeholder="Mail/Femail" name="gender" required> <br>
+     
+    
+    <br>
+     Email : 
+     <input type="email" placeholder="abcd@abd.abc" name="email" required> <br><br>
+     
+    
+     Date of Birth : 
+     <input type="text" placeholder="dd/MM/YYYY" name="birth" required> <br><br>
+    
+    <tr>
+      <td>
+
+      <input type="submit" value="Submit">
+      </td>
+    </tr>
+     
+    
+</form>
+
+</center>
 </body>
 </html>
